895033480b0a76b8af64a1abc6bbf2c77b5ed3f1
ParthRoot <parthitadara@gmail.com>
2024-11-09 16:58:23 +0530

    crud api

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const productRoute = require("./routes/product.route.js");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello This is node api");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin123@node-crud.uuodp.mongodb.net/test-crud?retryWrites=true&w=majority&appName=Node-CRUD"
+  )
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(3000, () => {
+      console.log("Server are running on PORT: 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
